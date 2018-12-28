c5aa52b96f14cb5215f28cbfb7bfe7646a1411d7
Arif Luthfi <arifluthfi16@gmail.com>
2018-12-28 08:24:29 +0700

    Getting ip

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(req.ip);
-  console.log(req.ip);
+  res.send(req.ip.substr(7));
+  console.log(req.ip.substr(7));
 });
 
 module.exports = router;
